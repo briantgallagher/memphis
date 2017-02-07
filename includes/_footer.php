9d997798d8efca681ddc33419858fcd7324e24a5
Brian T. Gallagher <bgallagher@gmail.com>
2017-02-06 20:15:32 -0500

    Update footer and add Github links

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,11 +1,17 @@
 		</div>
 
 		<footer>
-			<p>
-				<span>&copy; <?php echo date('Y'); ?></span>
-				<img src="/images/stars.svg" alt="stars" />
-			</p>
+			<div class="footer-wrap">
+				<p class="footer-copyright"><span>&copy; <?php echo date('Y'); ?></span></p>
 
+				<div class="footer-social">
+					<p>
+						<a href="http://www.linkedin.com/in/btgallagher"><span class="icon icon-linkedin"></span></a> 
+						<a href="https://github.com/briantgallagher"><span class="icon icon-github"></span></a>
+						<a href="http://briantgallagher.tumblr.com"><span class="icon icon-tumblr"></span></a>
+					</p>
+				</div>
+			</div>
 		</footer>
 
 		<script type="text/javascript" src="/scripts/utilities.js"></script>
