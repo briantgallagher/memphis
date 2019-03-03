0110013e3283d1a0cbed13b703db8cb45b7e8dbc
Brian T. Gallagher <bgallagher@gmail.com>
2019-03-03 15:40:22 -0500

    Updates to Amoskeag Flag text

--- work/amoskeag-flag.php
+++ work/amoskeag-flag.php
@@ -61,11 +61,11 @@
 						</div>
 
 						<div class="content-description">
-							<p>In early 2017, my hometown of Manchester, New Hampshire held a competition seeking designs for a city flag.  One of my entries, nicknamed the Amoskeag Flag, was selected as a finalist.  The contest received coverage through local news outlets and <a href="https://www.bostonglobe.com/arts/2017/06/13/stiff-winds-city-halls-for-would-flag-designers/Pk7ryXTsUgaUMZVAHxcRDJ/story.html">The Boston Globe</a> and even appeared in a downtown mural.</p>
+							<p>In early 2017, my hometown of Manchester, New Hampshire held a competition seeking designs for a city flag.  One of my entries, nicknamed the Amoskeag Flag, was selected as a finalist.  The contest received coverage through local news outlets, <a href="https://www.bostonglobe.com/arts/2017/06/13/stiff-winds-city-halls-for-would-flag-designers/Pk7ryXTsUgaUMZVAHxcRDJ/story.html">The Boston Globe</a> and even appeared in a downtown mural.</p>
 
-							<p>The design and color palette reference the city's natural features, highlighting Amoskeag Falls, a waterfall central to early native settlements and 19th century industrialization, the Merrimack River and the region's surrounding forests.</p>
+							<p>The design and color palette reference the city's natural features.  These include Amoskeag Falls, a waterfall central to early native settlements and 19th century industrialization, the Merrimack River and the region's surrounding forests.</p>
 
-							<p>The flag was supported by a promotional campaign on Facebook which garnered over 23,000 impressions.  It consisted of content detailing the design, creative process and additional posts traveling the city to explore local landmarks.</p>
+							<p>The flag was supported by a promotional campaign on Facebook which garnered over 23,000 impressions.  It consisted of content detailing the design and creative process along with supplementary posts exploring local landmarks and historical spots.</p>
 
 							<p>Additionally, four other designs among my submissions reached the final round of judging.</p>
 						</div>
